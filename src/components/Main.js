77500cb0e96e8c8fcea725bc8adcd1d923d12365
Minajeanne <albrown07@mac.com>
2020-04-16 13:03:43 -0400

    *

--- src/components/Main.js
+++ src/components/Main.js
@@ -110,7 +110,7 @@ class Main extends React.Component {
             <li>
               <a href="https://rubygems.org/profiles/alicialsantiago"
                 target="_blank" rel="noopener noreferrer"
-                className="icon fa-gem">
+                className="fas fa-gem">
                 <span className="label">RubyGems</span>
               </a>
             </li>
